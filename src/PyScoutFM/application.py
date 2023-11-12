4ee5c3b66dbadc84358f56bcb90c92ec266c3099
Oli Morris <olimorris@users.noreply.github.com>
2023-11-12 22:43:38 +0000

    wip

--- src/PyScoutFM/application.py
+++ src/PyScoutFM/application.py
@@ -1,10 +1,36 @@
 import typer
+from typing_extensions import Annotated
 
-app = typer.Typer()
+app = typer.Typer(add_completion=False)
+
+
+def validate(config):
+    return True
 
 
 @app.command()
-def main(config: str):
+# TODO: Enable users to pass config params via CLI
+def main(
+    config: Annotated[
+        str, typer.Argument(help="The path to the config file to use")
+    ] = "config.json"
+):
+    """
+    PyScoutFM
+
+    Use the power of Python to traverse the cosmos and evaluate players within the Football Manager series
+    """
+    if validate(config):
+        typer.echo("Validated")
+
+    # Validate the config file
+
+    # Load the config file
+
+    # Begin computing the player ratings
+
+    # Generate the output
+
     print(f"Hello {config}")
 
 
