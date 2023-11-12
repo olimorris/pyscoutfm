bd2b9befd803e1981bec3dd4147561dd20b38cee
Oli Morris <olimorris@users.noreply.github.com>
2023-11-12 14:23:54 +0000

    move to typer and pyproject.toml

--- src/PyScoutFM/application.py
+++ src/PyScoutFM/application.py
@@ -1,42 +1,12 @@
-import json
+import typer
+
+app = typer.Typer()
 
 
-def process_data(config, import_path, export_path, ratings_path, ratings_set):
-    """
-    Your core application logic will go here.
-    This function can be expanded based on what processing you need to do.
-    """
-    # Placeholder for processing logic
-    print(f"Processing data with import path: {import_path}")
-    print(f"Exporting data to: {export_path}")
-    print(f"Using ratings from: {ratings_path}")
-    print(f"Ratings set selected: {ratings_set}")
-
-    # Process data
-    with open(config, "r", encoding="utf-8") as file:
-        config = json.load(file)
-
-    config["import_path"] = import_path or config["import_path"]
-    config["export_path"] = export_path or config["export_path"]
-    config["ratings_path"] = ratings_path or config["ratings_path"]
-    config["ratings_set"] = ratings_set or config["ratings_set"]
-    # ...
-
-    result = "Data processing complete."  # Placeholder for actual result
-    return result
-
-
-def main():
-    """
-    The main entry point for the application.
-    It reads the configuration, processes command line arguments, and starts the main process.
-    """
-    # Here you can read the configuration file and update your application's settings
-    # ...
-
-    # Call your core processing function with the paths
-    print("Nice")
+@app.command()
+def main(config: str):
+    print(f"Hello {config}")
 
 
 if __name__ == "__main__":
-    main()
+    app()
