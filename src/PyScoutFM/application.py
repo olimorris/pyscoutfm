cf27185fb4976c3b5807e3c2c77be23630847706
Oli Morris <olimorris@users.noreply.github.com>
2023-11-20 22:26:02 +0000

    add version output command

--- src/PyScoutFM/application.py
+++ src/PyScoutFM/application.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 import typer
 from typing_extensions import Annotated
 
+from PyScoutFM import __version__
 from PyScoutFM.data import Data
 from PyScoutFM.formatter import Formatter
 from PyScoutFM.generator import Generator
@@ -9,18 +12,29 @@ from PyScoutFM.importer import Importer
 app = typer.Typer(add_completion=False)
 
 
+def version_callback(value: bool):
+    if value:
+        print(f"PyScoutFM {__version__}")
+        raise typer.Exit()
+
+
 @app.command()
 # TODO: Enable users to pass config params via CLI
 def main(
     config: Annotated[
         str, typer.Argument(help="The path to the config file to use")
-    ] = "config.json"
+    ] = "config.json",
+    version: Annotated[
+        Optional[bool],
+        typer.Option("--version", "-V", callback=version_callback, is_eager=True),
+    ] = None,
 ):
     """
     PyScoutFM
 
     Use the power of Python to traverse the cosmos and scout players within the Football Manager series
     """
+
     # Load and process the config file
     importer = Importer(config)
     ratings = importer.load_ratings()
